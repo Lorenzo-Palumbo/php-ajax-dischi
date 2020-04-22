13cc0d24966c7d677f7a2bc440c8d09936bdb74b
Unknown <lorenzo.palumbo@hotmail.com>
2020-04-22 09:36:22 +0200

    Ajax

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,24 @@
 $(document).ready(function (){
 
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+
     $.ajax({
         url:'server.php',
         method: 'GET',
         success: function(data){
             var albums = JSON.parse(data);
-            console.log(dati);
             for (var i = 0; i < albums.length; i++) {
                 var album = albums[i];
+                console.log(album);
+                var context = {
+                    img: album.image,
+                    album: album.album_name,
+                    artist: album.artist_name,
+                    year: album.album_year
+                 };
+                var html = template(context);
+                $('main').append(html);
             }
         },
         error: function(error){
--- index-ajax.php
+++ index-ajax.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="dist/css/app.css">
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js"></script>
         <title>php-ajax-dischi</title>
     </head>
     <body>
@@ -17,18 +18,21 @@
         </header>
         <div class="container-full">
             <main>
-                 <div class="card">
-                     <div class="card-img">
-                         <img src="" alt="">
-                     </div>
-                     <div class="card-bottom">
-                         <h3></h3>
-                         <h4></h4>
-                         <small></small>
-                     </div>
-                 </div>
+
             </main>
         </div>
+        <script id="entry-template" type="text/x-handlebars-template">
+            <div class="card">
+                <div class="card-img">
+                    <img src="{{img}}" alt="">
+                </div>
+                <div class="card-bottom">
+                    <h3>{{album}}</h3>
+                    <h4>{{artist}}</h4>
+                    <small>{{year}}</small>
+                </div>
+            </div>
+        </script>
 
         <script src="dist/js/main.js" charset="utf-8"></script>
     </body>
